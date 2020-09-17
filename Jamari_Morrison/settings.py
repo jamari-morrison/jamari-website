b2bb4ad21f2d36c2892ac5e1b08ab76a287c4a48
Morrison <morrisjj@rose-hulman.edu>
2020-09-16 20:08:23 -0400

    Updated resume, css, and HTML

--- Jamari_Morrison/settings.py
+++ Jamari_Morrison/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 import django_heroku
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '3i5n26srvlt4=#^_@%lt8cqn)3%bzpp0#dwe9cf_&(n$=v=kuv'
+SECRET_KEY = os.environ.get('DJANGO_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
