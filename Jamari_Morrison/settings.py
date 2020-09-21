ec6a29a231d560d3d15437100594133773d4e9c5
Morrison <morrisjj@rose-hulman.edu>
2020-09-21 19:55:59 -0400

    Updated allowed_hosts

--- Jamari_Morrison/settings.py
+++ Jamari_Morrison/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('DJANGO_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.localhost', '127.0.0.1', '[::1]', 'www.jamari.tech', 'https://jamari-morrison.herokuapp.com/']
 
 
 # Application definition
