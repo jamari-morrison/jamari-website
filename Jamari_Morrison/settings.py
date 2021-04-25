81b5b239d40982da1801c6c5b74a61196c62468c
jamari-morrison <RedCater360@yahoo.com>
2021-04-24 22:54:47 -0400

    Fixing email

--- Jamari_Morrison/settings.py
+++ Jamari_Morrison/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('DJANGO_KEY')
 
-# SECURITY WARNING: don't run with debug turned on in production!
+# SECURITY WARNING: don't run with debu g turned on in production!
 DEBUG = False
 
 ALLOWED_HOSTS = [
